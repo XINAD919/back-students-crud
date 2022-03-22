d32c93896b4492dc4f04cb67f3b4a8f6c387b0a1
Daniel <Da_919@hotmail.com>
2022-03-22 01:40:07 -0500

    se crea modelo y schema de tipo student y se hace un crud de prueba

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import { conectarDB } from "./db/db";
+import { studentModel } from "./models/Student/student";
 
 const app = express();
 
@@ -14,8 +15,69 @@ dotenv.config({
 app.use(express.json());
 app.use(cors());
 
-app.listen({ port: port }, async() => {
+app.listen({ port: port }, async () => {
   console.log("Server is running on port", process.env.PORT);
   await conectarDB();
+
+  // CREAR
+  // const crearEstudiante = await studentModel
+  //   .create({
+  //     nombre: "paola Castaño",
+  //     identificacion: "43582493",
+  //     correo: "pc.barrera@hotmail.com",
+  //     tel: 32456789,
+  //   })
+  //   .then((u) => {
+  //     console.log("estudiante creado", u);
+  //   })
+  //   .catch((err) => {
+  //     console.error("error al crear estudiante", err);
+  //   });
+
+  // CONSULTAR TODOS
+  // const consultarEstudiantes = await studentModel
+  //   .find()
+  //   .then((u) => {
+  //     console.log("usuarios", u);
+  //   })
+  //   .catch((e) => {
+  //     console.error("error encontrando estudiantes", e);
+  //   });
+
+  // CONSULTAR POR ID
+  // const consultarEstudiante = await studentModel
+  //   .findOne({ correo: "da_919@hotmail.com" })
+  //   .then((u) => {
+  //     console.log("estudiante encontrado", u);
+  //   })
+  //   .catch((e) => {
+  //     console.error("error encontrando estudiante", e);
+  //   });
+
+  // ACTUALIZAR
+  // const actualizarEstudiante = await studentModel
+  //   .findOneAndUpdate(
+  //     { correo: "da_919@hotmail.com" },
+  //     { nombre: "Daniel Castaño Barrera" },
+  //     {
+  //       new: true,
+  //     }
+  //   )
+  //   .then((u) => {
+  //     console.log("estudiante actualizado", u);
+  //   })
+  //   .catch((e) => {
+  //     console.error("error actualizando estudiante", e);
+  //   });
+
+  // ELIMINAR
+  // const eliminarEstudiante = await studentModel
+  //   .findOneAndDelete({ correo: "xinad919@gmail.com" })
+  //   .then((u) => {
+  //     console.log("estudiante eliminado con exito", u);
+  //   })
+  //   .catch((e) => {
+  //     console.error("error eliminando estudiante", e);
+  //   });
+
 });
- 
--- models/Student/student.ts
+++ models/Student/student.ts
@@ -1,3 +1,40 @@
-
+import { model, Schema } from "mongoose";
 interface Student {
+  nombre: string;
+  identificacion: string;
+  correo: string;
+  tel: number;
 }
+
+const studentSchema = new Schema<Student>({
+  nombre: {
+    type: String,
+    required: true,
+  },
+  identificacion: {
+    type: String,
+    required: true,
+    unique:true,
+  },
+  correo: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: (value: string) => {
+        return /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/.test(
+          value
+        );
+      },
+      message: "ingresa un correo valido",
+    },
+  },
+  tel: {
+    type: Number,
+    required: true,
+    unique: true,
+  },
+});
+const studentModel=model("student",studentSchema);
+
+export {studentModel};
